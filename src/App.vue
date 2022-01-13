4c646cb81bff225d1741d38798e634b2dc62e85c
Saba Tahir <sata@ciklum.com>
2022-01-13 12:01:04 +0500

    implemented covid 19 tracker with vue 3 &tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+<div>
+  <Header/>
+<div class="container">
   <router-view/>
+     </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+import Header from './components/Header.vue'
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+export default {
+components: {
+   Header
+  },
 }
-</style>
+ 
+
+</script>
---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+       <select v-model="selected" 
+       @change="onChange"
+       class="form-select mt-10 block w-full border p-3 rounded">
+           <option value="0">Select Country</option>
+           <option v-for="country in countries" :value="country.ID" :key="country.id">
+               {{country.Country}}
+           </option>
+       </select>
+    </div>
+</template>
+<script>
+export default {
+    name:'CountrySelect',
+    props:['countries'],
+    data(){
+        return{
+            selected: 0
+        }
+    },
+    methods:{
+        onChange(){
+            const country= this.countries.find((item)=>item.ID === this.selected)
+            this.$emit('select-country',country)
+        }
+    }
+}
+</script>
